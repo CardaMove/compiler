05e58300a3eca240479e0e69579299043bf3ae03
roveroniandrea <roveroniandrea@gmail.com>
2025-07-23 12:09:04 +0200

    some lexer tests

--- test/Move/LexerSpec.hs
+++ test/Move/LexerSpec.hs
@@ -8,38 +8,60 @@ testScan :: String -> [Token] -> SpecWith ()
 testScan str toks = it str $ scan str `shouldBe` toks
 
 testScanBraces :: Spec
-testScanBraces = describe "Parse {}" $ do
+testScanBraces = describe "Scan {}" $ do
   testScan "{}" [TokenSeparatorLBrace, TokenSeparatorRBrace]
   testScan "{ }" [TokenSeparatorLBrace, TokenSeparatorRBrace]
   testScan "{ } " [TokenSeparatorLBrace, TokenSeparatorRBrace]
   testScan " { }" [TokenSeparatorLBrace, TokenSeparatorRBrace]
 
 testScanModule :: Spec
-testScanModule = describe "Parse module" $ do
+testScanModule = describe "Scan module" $ do
   testScan "module" [TokenKeywordModule]
   testScan "module " [TokenKeywordModule]
   testScan "module {" [TokenKeywordModule, TokenSeparatorLBrace]
-  testScan "module culo {}" [TokenKeywordModule, TokenIdentifier "culo", TokenSeparatorLBrace, TokenSeparatorRBrace]
+  testScan "module CAFFE {}" [TokenKeywordModule, TokenIdentifier "CAFFE", TokenSeparatorLBrace, TokenSeparatorRBrace]
   testScan "module abc::def {}" [TokenKeywordModule, TokenIdentifier "abc", TokenSeparatorDColon, TokenIdentifier "def", TokenSeparatorLBrace, TokenSeparatorRBrace]
-  testScan "module 0xABCD::culo {}" [TokenKeywordModule, TokenLiteralIntHex "0xABCD", TokenSeparatorDColon, TokenIdentifier "culo", TokenSeparatorLBrace, TokenSeparatorRBrace]
+  testScan "module 0xABCD::CAFFE {}" [TokenKeywordModule, TokenLiteralIntHex "0xABCD", TokenSeparatorDColon, TokenIdentifier "CAFFE", TokenSeparatorLBrace, TokenSeparatorRBrace]
 
 testScanDecimal :: Spec
-testScanDecimal = describe "Parse decimals" $ do
+testScanDecimal = describe "Scan decimals" $ do
   testScan "0" [TokenLiteralIntDec 0]
   testScan "1" [TokenLiteralIntDec 1]
   testScan "123" [TokenLiteralIntDec 123]
 
 testScanHex :: Spec
-testScanHex = describe "Parse hexadecimals" $ do
+testScanHex = describe "Scan hexadecimals" $ do
   testScan "0x0" [TokenLiteralIntHex "0x0"]
   testScan "0x1" [TokenLiteralIntHex "0x1"]
   testScan "0x123" [TokenLiteralIntHex "0x123"]
   testScan "0xFF2E" [TokenLiteralIntHex "0xFF2E"]
 
 testScanString :: Spec
-testScanString = describe "Parse strings" $ do
+testScanString = describe "Scan strings" $ do
   testScan "\"\"" [TokenLiteralString "\"\""]
   testScan "\"hello\"" [TokenLiteralString "\"hello\""]
+  -- TODO: Incomplete string definition (see snake_case)
+  -- testScan "\"123_string0xABC\"" [TokenLiteralString "\"123_string0xABC\""]
+
+testScanBool :: Spec
+testScanBool = describe "Scan booleans" $ do
+  testScan "true" [TokenLiteralBool True]
+  testScan "false" [TokenLiteralBool False]
+
+testScanBinding :: Spec
+testScanBinding = describe "Scan bindings" $ do
+  testScan "let answer = 42" [TokenKeywordLet, TokenIdentifier "answer", TokenOperatorAssign, TokenLiteralIntDec 42]
+
+testScanIdentifier :: Spec
+testScanIdentifier = describe "Scan identifiers" $ do
+  testScan "answer" [TokenIdentifier "answer"]
+  testScan "camelCase" [TokenIdentifier "camelCase"]
+  testScan "PascalCase" [TokenIdentifier "PascalCase"]
+  -- TODO: Incomplete string definition
+  -- testScan "snake_case" [TokenIdentifier "snake_case"]
+  testScan "myAnswer0" [TokenIdentifier "myAnswer0"]
+  testScan "letIdentifier" [TokenIdentifier "letIdentifier"]
+  testScan "trueVariable" [TokenIdentifier "trueVariable"]
 
 spec :: Spec
 spec = do
@@ -48,3 +70,6 @@ spec = do
   testScanDecimal
   testScanHex
   testScanString
+  testScanBool
+  testScanBinding
+  testScanIdentifier
