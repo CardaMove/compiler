decade4b4ebf7d734888e94a10952827a4c7782d
roveroniandrea <roveroniandrea@gmail.com>
2025-08-16 12:37:44 +0200

    added loop translation test for nested loops

--- test/Move/Translations/LoopsSpec.hs
+++ test/Move/Translations/LoopsSpec.hs
@@ -7,7 +7,7 @@ import Test.Hspec
 testLoopsToWhile :: Spec
 testLoopsToWhile = describe "Translating a loop into a while" $ do
     it "Translates a Loop Expr" $ do
-        let body = SequenceExpr $ Sequence {
+        let loopBody = SequenceExpr $ Sequence {
             sequenceUses = [],
             sequenceItems = [
                 -- let a = 10
@@ -23,9 +23,75 @@ testLoopsToWhile = describe "Translating a loop into a while" $ do
             sequenceEndExpr = Just $ Return $ Just $ NameAccessChainExpr $ LocalNameAccessChain $ Identifier "a"
         }
 
-        (mapLoopsToWhile $ Loop body) `shouldBe` (WhileTerm $ While {
+        (mapLoopsToWhile $ Loop loopBody) `shouldBe` (WhileTerm $ While {
             whileCondition = ValueLiteral $ Boolean True,
-            whileExpr = body
+            whileExpr = loopBody
+        })
+
+
+    it "Translates nested Loop expressions" $ do
+        let innerLoopBody = SequenceExpr $ Sequence {
+            sequenceUses = [],
+            sequenceItems = [],
+            -- a = a + 1
+            sequenceEndExpr = Just $ AssignmentExpr $ Assignment {
+                assignmentLeft = NameAccessChainExpr $ LocalNameAccessChain $ Identifier "a",
+                assignmentRight = BinaryOpExprExpr $ Add
+                    (NameAccessChainExpr $ LocalNameAccessChain $ Identifier "a")
+                    (ValueLiteral $ Numerical $ LiteralIntDec 1)
+            }
+        }
+
+        let functionBody = Sequence {
+            sequenceUses = [],
+            sequenceItems = [],
+            -- outer loop
+            sequenceEndExpr = Just $ Loop $ SequenceExpr $ Sequence {
+                sequenceUses = [],
+                sequenceItems = [
+                    -- let a = 10
+                    SequenceItemBindExpr $ Bindings {
+                        bindings = [
+                            BindIdentifier $ Identifier "a"
+                        ],
+                        bindingsBindType = Nothing,
+                        bindingsBindExpr = Just $ ValueLiteral $ Numerical $ LiteralIntDec 10
+                    },
+                    -- inner loop
+                    SequenceItemExpr $ Loop $ innerLoopBody
+                ],
+                -- return a
+                sequenceEndExpr = Just $ Return $ Just $ NameAccessChainExpr $ LocalNameAccessChain $ Identifier "a"
+            }
+        }
+
+        (mapLoopsToWhile $ functionBody) `shouldBe` (Sequence {
+            sequenceUses = [],
+            sequenceItems = [],
+            -- outer loop remapped in while
+            sequenceEndExpr = Just $ WhileTerm $ While {
+                whileCondition = ValueLiteral $ Boolean True,
+                whileExpr = SequenceExpr $ Sequence {
+                    sequenceUses = [],
+                    sequenceItems = [
+                        -- let a = 10
+                        SequenceItemBindExpr $ Bindings {
+                            bindings = [
+                                BindIdentifier $ Identifier "a"
+                            ],
+                            bindingsBindType = Nothing,
+                            bindingsBindExpr = Just $ ValueLiteral $ Numerical $ LiteralIntDec 10
+                        },
+                        -- inner loop remapped in while
+                        SequenceItemExpr $ WhileTerm $ While {
+                            whileCondition = ValueLiteral $ Boolean True,
+                            whileExpr = innerLoopBody
+                        }
+                    ],
+                    -- return a
+                    sequenceEndExpr = Just $ Return $ Just $ NameAccessChainExpr $ LocalNameAccessChain $ Identifier "a"
+                }
+            }
         })
 
 spec :: Spec
