f1f408f67b6cdf5ee0aa323e7201176dfbea7794
roveroniandrea <roveroniandrea@gmail.com>
2025-08-19 19:22:07 +0200

    comments

--- lib/Move/Translations/Loops.hs
+++ lib/Move/Translations/Loops.hs
@@ -6,9 +6,10 @@ import Data.Data (Data)
 import Data.Generics.Uniplate.Data (transformBi)
 import Move.AST (Expr (..), ValueLiteral (Boolean), While (..))
 
--- Need to declare that the function only works on data types deriving Data
--- | Translates all `loop expr` expressions into `while(true) expr`
--- | Works recursively on any node of the AST
+-- |
+-- Translates all `loop expr` expressions into `while(true) expr`.
+--
+-- Works recursively on any node of the AST
 mapLoopsToWhile :: (Data from) => from -> from
 mapLoopsToWhile = transformBi f
   where
