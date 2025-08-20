64f52f18b02b39505ebae41301b359c7b07d0258
roveroniandrea <roveroniandrea@gmail.com>
2025-08-20 11:21:43 +0200

    shadowing considers uses and aliases, existing tests still work

--- lib/Move/AST.hs
+++ lib/Move/AST.hs
@@ -154,7 +154,9 @@ data Use
   = Use
   { useAddress :: Address,
     useIdentifier :: Identifier,
+    -- | If the alias is present, no members can be aliased and vice-versa
     useAlias :: Maybe Identifier,
+    -- | If the alias is present, no members can be aliased and vice-versa
     useMembers :: [UseMember]
   }
   deriving (Eq, Show, Data, Typeable)
